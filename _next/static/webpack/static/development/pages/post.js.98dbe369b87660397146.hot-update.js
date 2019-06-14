b2fd601b621ca487db7b76ca044594b6ee8fd9f4
phiresky <phireskyde+git@gmail.com>
2019-06-14 13:46:44 +0200

    Revert "del"
    This reverts commit c1d07212d81cb368962ca15f70a14aea1d07dfaa.

---
+++ _next/static/webpack/static/development/pages/post.js.98dbe369b87660397146.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/post.js",{
+
+/***/ "./src/components/Page.tsx":
+/*!*********************************!*\
+  !*** ./src/components/Page.tsx ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_objectSpread__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/objectSpread */ "./node_modules/@babel/runtime-corejs2/helpers/esm/objectSpread.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Header__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Header */ "./src/components/Header.tsx");
+/* harmony import */ var _Hero__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Hero */ "./src/components/Hero.tsx");
+/* harmony import */ var _Footer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./Footer */ "./src/components/Footer.tsx");
+/* harmony import */ var _Tracking__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./Tracking */ "./src/components/Tracking.tsx");
+/* harmony import */ var _config__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../config */ "./src/config.ts");
+
+
+
+
+
+
+ // rrtod body = children
+
+function Page(_props) {
+  var props = Object(_babel_runtime_corejs2_helpers_esm_objectSpread__WEBPACK_IMPORTED_MODULE_0__["default"])({}, _config__WEBPACK_IMPORTED_MODULE_6__["config"], _props);
+
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Header__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    siteTitle: (props.title ? props.title + " - " : "") + props.siteTitle,
+    description: props.description,
+    stylesheets: props.stylesheets
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("main", {
+    className: "lh-copy"
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Hero__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    heroTitle: props.siteTitle,
+    subtitle: props.description,
+    topLinks: props.topLinks,
+    backgroundClass: props.backgroundClass
+  }), props.children, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Footer__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    copyright: props.copyright
+  }), props.siteId && react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Tracking__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    siteId: props.siteId
+  })));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Page);
+
+/***/ })
+
+})
+//# sourceMappingURL=post.js.98dbe369b87660397146.hot-update.js.map
