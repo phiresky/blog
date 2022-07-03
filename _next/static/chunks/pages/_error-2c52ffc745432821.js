f56d603483b0f5065bc493f54184d93a700ecd59
phiresky <phireskyde+git@gmail.com>
2022-07-03 15:04:20 +0200

    update binaries

---
+++ _next/static/chunks/pages/_error-2c52ffc745432821.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{5832:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return c(3595)}])}},function(a){a.O(0,[888,774,179],function(){var b;return a(a.s=5832)}),_N_E=a.O()}])
